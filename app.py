18d8e319038cb484c27907b6648a21e248dc4f76
crycetruly <crycetruly@gmail.com>
2018-12-06 13:29:01 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def get_formatted_address(add):
+    """
+    calculates the latlong given an address
+    :param add:
+    :return:
+    """
+    try:
+        r = requests.get(
+             "https://maps.googleapis.com/maps/api/geocode/json?address="+add+"&key=AIzaSyDQQ3v45Vf1LVh2JZFwh4yHaM4ERoPf1M0")
+        data = r.json()
+        results= data['results']
+        address=results[0]
+        return  address['formatted_address']
+    except Exception as identifier:
+        return None
+
+
+
+print(get_formatted_address('Jinja'));
