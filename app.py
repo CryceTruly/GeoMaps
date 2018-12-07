aa3c1f36de51418a6f3f2d2f6ca2d20efa5c7b94
crycetruly <crycetruly@gmail.com>
2018-12-07 18:20:58 +0300

    refactored request

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def get_formatted_address(add):
 
     try:
         r = requests.get(
-             "https://maps.googleapis.com/maps/api/geocode/json?address="+add+"&key=AbIzaSyDQQ3v45Vf1LVh2JZFwh4yHaM4ERoPf1M0")
+             "https://maps.googleapis.com/maps/api/geocode/json?address="+add+"&key=AIzaSyDQQ3v45Vf1LVh2JZFwh4yHaM4ERoPf1M0")
         data = r.json()
         results= data['results']
         address=results[0]
@@ -18,6 +18,47 @@ def get_formatted_address(add):
     except Exception as identifier:
         return None
 
+def get_latlng(add):
+    '''
+
+    :param add:
+    :return:
+    '''
+
+    try:
+        r = requests.get(
+             "https://maps.googleapis.com/maps/api/geocode/json?address="+add+"&key=yourkey")
+        data = r.json()
+        results= data['results']
+        address=results[0]
+        # Geometry
+        geometry= address['geometry']
+        return geometry['location']
+    except Exception as identifier:
+        return {'lat': 37.06250000000001, 'lng': -95.677068}
+
+def get_dest(add):
+    '''
+
+    :param add:
+    :return:
+    '''
+
+    try:
+        r = requests.get(
+             "https://maps.googleapis.com/maps/api/geocode/json?address="+add+"&key=yourkey")
+        data = r.json()
+        results= data['results']
+        address=results[0]
+        # Geometry
+        geometry= address['geometry']
+        return geometry['location']
+    except Exception as identifier:
+        return {'lat': 0.3475964, 'lng': 32.5825197}
 
 
-print(get_formatted_address('Jinja'));
+
+
+
+
+print(get_latlng('Kampala'));
